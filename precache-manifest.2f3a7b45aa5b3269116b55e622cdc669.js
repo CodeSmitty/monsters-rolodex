e65b49ef3e8060a8f0746546ffc34168aa49e8bd
codesmitty <varcodesmitty@gmail.com>
2020-06-06 11:03:12 -0400

    Updates

---
+++ precache-manifest.2f3a7b45aa5b3269116b55e622cdc669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c14bc7303c7fccd90844de768ecda31",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "85d4eb7a7cf07abd11d0",
+    "url": "/monsters-rolodex/static/css/main.6f8e2cb9.chunk.css"
+  },
+  {
+    "revision": "c16ade49ed63e63d06da",
+    "url": "/monsters-rolodex/static/js/2.c61e2a3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.c61e2a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d4eb7a7cf07abd11d0",
+    "url": "/monsters-rolodex/static/js/main.e233d881.chunk.js"
+  },
+  {
+    "revision": "ee323659206c40587cb2",
+    "url": "/monsters-rolodex/static/js/runtime-main.a212e1bf.js"
+  }
+]);
